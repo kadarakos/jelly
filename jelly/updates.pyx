5e135455f5438dd815ffb8c5aa9c978f2fedc082
kadarakos <kadar.akos@gmail.com>
2023-07-14 14:18:52 +0200

    faster indexing

--- jelly/updates.pyx
+++ jelly/updates.pyx
@@ -5,9 +5,10 @@ import numpy as np
 from .rules cimport cyclic_rule
 from .typedefs cimport DTYPE_t, IMGTYPE_t
 from .ty import DTYPE
-from .cyhoods cimport hood_by_name, hood_size, moore
+from .cyhoods cimport hood_by_name, hood_size
 cimport numpy as cnp
 import cython
+
 
 
 @cython.boundscheck(False)
@@ -17,33 +18,35 @@ def cyclic_step(
     const DTYPE_t[:, :] C_padded,
     int n_states,
     str neighborhood,
-    int neighborhood_size,
-    const IMGTYPE_t[:, :] cmap,
+    DTYPE_t[:] neighbors,
+    IMGTYPE_t[:, :] cmap,
     int threshold,
     int size,
-    const IMGTYPE_t[:, :, :] out_rgb
+    DTYPE_t[:, :] out,
+    IMGTYPE_t[:, :, :] out_rgb
 ):
     neighborhood_func = hood_by_name(neighborhood)
     height = C_padded.shape[0]
     width = C_padded.shape[1]
-    out = np.empty((height, width), dtype=DTYPE)
-    neighbors = np.empty((neighborhood_size, ), dtype=DTYPE)
-    cdef DTYPE_t[:, :] out_view = out
-    cdef DTYPE_t[:] neighbors_view = neighbors
     cdef DTYPE_t value
     cdef DTYPE_t new_value
     cdef int cmap_size = cmap.shape[0]
-    cdef int i, j
+    cdef int i, j, rgb_i, rgb_j
+    cdef int color
     for i in range(height):
         for j in range(width):
             value = C_padded[i, j]
-            # Padding
             if value == -1:
-                new_value = -1
-                out_view[i, j] = new_value
+                out[i, j] = value
             else:
-                moore(C_padded, i, j, size, neighbors_view)
-                new_value = cyclic_rule(value, neighbors_view, n_states, threshold)
-                out_view[i, j] = new_value
-                out_rgb[i - size, j - size, :] = cmap[new_value % cmap_size]
+                rgb_i = i - size
+                rgb_j = j - size
+                neighborhood_func(C_padded, i, j, size, neighbors)
+                new_value = cyclic_rule(value, neighbors, n_states, threshold)
+                out[i, j] = new_value
+                # Stupid way of writing because slicing seems slow?
+                color = new_value % cmap_size
+                out_rgb[rgb_i, rgb_j, 0] = cmap[color][0]
+                out_rgb[rgb_i, rgb_j, 1] = cmap[color][1]
+                out_rgb[rgb_i, rgb_j, 2] = cmap[color][2]
     return out
