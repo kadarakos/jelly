e4aff8f5eac96e54e756f6e3c8577caffc10fabf
kadarakos <kadar.akos@gmail.com>
2023-07-17 12:57:00 +0200

    lifelike rule

--- jelly/rules.pyx
+++ jelly/rules.pyx
@@ -1,5 +1,6 @@
 # cython: binding=True, infer_types=True
 cimport numpy as cnp
+import numpy as np
 from .typedefs cimport DTYPE_t
 import cython
 
@@ -23,5 +24,26 @@ cdef DTYPE_t cyclic_rule(int value, DTYPE_t[:] neighbors, int n_states, int thre
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def game_of_life():
-    ...
+cdef DTYPE_t lifelike_rule(int value, DTYPE_t[:] neighbors, DTYPE_t[:] B, DTYPE_t[:] S) nogil:
+    # Padding
+    if value == -1:
+        return -1
+    cdef int alive = 0
+    cdef Py_ssize_t neighbors_size = neighbors.shape[0]
+    cdef Py_ssize_t B_size = B.shape[0]
+    cdef Py_ssize_t S_size = S.shape[0]
+    for i in range(neighbors_size):
+        if neighbors[i] == 1:
+            alive += 1
+    # Cell is dead.
+    if value == 0:
+        for i in range(B_size):
+            if alive == B[i]:
+                return 1
+        return 0
+    # Cell is alive.
+    else:
+        for i in range(S_size):
+            if alive == S[i]:
+                return 1
+        return 0
