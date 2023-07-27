6e730639c959915e0880c016782bccf6e343b351
kadarakos <kadar.akos@gmail.com>
2023-07-27 22:48:12 +0200

    dont need custom enum anymore

--- jelly/util.py
+++ jelly/util.py
@@ -1,21 +1,5 @@
-from enum import Enum
-
 import numpy as np
-
 from .ty import IMGTYPE
-
-
-class EnumChoices:
-    enumeration: Enum
-    options: Enum
-
-    def __init__(self, name: str, enumeration: Enum):
-        self.enumeration = enumeration
-        options = {x.name: x.name for x in enumeration}
-        self.options = Enum(name, options)
-
-    def resolve(self, option: str):
-        return getattr(self.enumeration, option).value
 
 
 def enlarge_img(arr: np.ndarray, ratio: int) -> np.ndarray:
